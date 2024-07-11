c629f87579ee8472846b98096d205e271b466159
WillAceNYC <wacevedo0313@gmail.com>
2024-07-11 15:23:04 -0400

    New Code Added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Daddy Chill");
+            Thread.sleep(2000);
+        }
+
+
     }
 }
